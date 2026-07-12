5a5943191b3af5dea2fcd239fdf0dd362c6c4fda
K3vnDev <kevinrpolo16@gmail.com>
2026-07-11 20:41:47 -0500

    fix breathing interaction in hide n seek

--- Scenes/Player/hiding_manager.gd
+++ Scenes/Player/hiding_manager.gd
@@ -1,17 +1,89 @@
 class_name PlayerHidingManager
 extends Node
 
+@export var player: Player
+@export var hiding_breathing: HidingBreathing
+@export var stop_hiding_timer: Timer
 
 var current_hide_interactable: HideInteractable
-
 var is_hiding: bool = false
 var hiding_left: float = 10.0
 var hide_timer: float = 0.0
 
+# This avoids multiple breathing corroutines to play at the same time
+var _breathing_generation := 0
+
+
+func _ready() -> void:
+	hiding_breathing.result_registered.connect(
+		_on_hiding_breathing_result_registered
+	)
+
 
 func _input(_event):
-	if Input.is_action_just_pressed(&"interact") and current_hide_interactable:
+	if (Input.is_action_just_pressed(&"interact") 
+		and current_hide_interactable):
+		
 		if not is_hiding:
-			current_hide_interactable.start_hiding()
-		else:
-			current_hide_interactable.stop_hiding()
+			start_hiding()
+			stop_hiding_timer.start()
+		
+		elif not stop_hiding_timer.time_left:
+			stop_hiding()
+
+
+func start_hiding() -> void:
+	var interactable := current_hide_interactable
+	if is_hiding or not interactable: return
+	
+	if interactable.current_ID == interactable.ID.one:
+		is_hiding = true
+		player.animated_sprite.visible = false
+		player.hiding_manager.is_hiding = true
+		player.global_position = interactable.global_position
+		player.can_move = false
+		player.velocity = Vector2.ZERO
+		player.set_interact_prompt(false)
+		
+		start_hiding_breathing()
+		
+	elif interactable.current_ID == interactable.ID.zero:
+		interactable.play_occuppied_dialog()
+
+
+func start_hiding_breathing() -> void:
+	_breathing_generation += 1
+	var this_breathing_generation = _breathing_generation
+	
+	while true:
+		await get_tree().create_timer(randf_range(2.0, 4.0)).timeout
+		
+		if not is_hiding or _breathing_generation != this_breathing_generation:
+			break
+		
+		hiding_breathing.execute()
+		await hiding_breathing.result_registered
+
+
+func _on_hiding_breathing_result_registered(result: HidingBreathing.TimingResult) -> void:
+	if not is_hiding: return
+	
+	if result in [HidingBreathing.TimingResult.Miss, HidingBreathing.TimingResult.Bad]:
+		stop_hiding()
+
+
+
+func stop_hiding() -> void:
+	var interactable := current_hide_interactable
+	if not is_hiding or not interactable: return
+
+	player.animated_sprite.visible = true
+	player.can_move = true
+	
+	hiding_left = 5.0
+	hide_timer = 0.0
+	is_hiding = false
+
+	# Move player to the front of the hiding place
+	interactable.move_player_back_to_front()
+	hiding_breathing.stop()
--- Script/Interactables/hide_interactable.gd
+++ Script/Interactables/hide_interactable.gd
@@ -12,7 +12,6 @@ enum PLACEMENT { left, right, up, down }
 
 var player: Player
 var _player_is_colliding := false
-var _player_is_hiding_here := false
 var _dialogue_node_id: String
 
 const STOP_HIDING_DISPLACEMENT := 35.0
@@ -25,20 +24,20 @@ func _ready() -> void:
 func _physics_process(delta: float):
 	time_passed += delta
 	
-	var total_IDS: int = ID.size()
-	var hiding_manager := player.hiding_manager
-	
-	if _player_is_colliding:
-		if hiding_manager.is_hiding:
-			hiding_manager.hide_timer += delta
-			hiding_manager.hiding_left -= delta
-		
-		if hiding_manager.hiding_left <= 0:
-			stop_hiding() 
-
-		if time_passed >= 30.0 and not hiding_manager.is_hiding:
-			time_passed -= 30.0
-			current_ID = (current_ID + 1) % total_IDS
+	#var total_IDS: int = ID.size()
+	#var hiding_manager := player.hiding_manager
+	#
+	#if _player_is_colliding:
+		#if hiding_manager.is_hiding:
+			#hiding_manager.hide_timer += delta
+			#hiding_manager.hiding_left -= delta
+		#
+		#if hiding_manager.hiding_left <= 0:
+			#stop_hiding() 
+#
+		#if time_passed >= 30.0 and not hiding_manager.is_hiding:
+			#time_passed -= 30.0
+			#current_ID = (current_ID + 1) % total_IDS
 
 
 func _on_body_entered(body: Node2D):
@@ -55,36 +54,14 @@ func _on_body_exited(body: Node2D):
 		player.hiding_manager.current_hide_interactable = null
 
 
-func start_hiding():
-	if player.hiding_manager.is_hiding: return
-	
-	if current_ID == ID.one:
-		_player_is_hiding_here = true
-		player.animated_sprite.visible = false
-		player.hiding_manager.is_hiding = true
-		player.global_position = global_position
-		player.can_move = false
-		player.velocity = Vector2.ZERO
-		player.set_interact_prompt(false)
+func play_occuppied_dialog() -> void:
+	if not _dialogue_node_id:
+		_dialogue_node_id = Set.pick_random()
 		
-	elif current_ID == ID.zero:
-		if not _dialogue_node_id:
-			_dialogue_node_id = Set.pick_random()
-		Dialogue.play("Hide_And_Seek", _dialogue_node_id)
+	Dialogue.play("Hide_And_Seek", _dialogue_node_id)
 
 
-func stop_hiding():
-	if not player.hiding_manager.is_hiding: return
-
-	_player_is_hiding_here = false
-	
-	player.animated_sprite.visible = true
-	player.hiding_manager.hiding_left = 5.0
-	player.hiding_manager.hide_timer = 0.0
-	player.hiding_manager.is_hiding = false
-	player.can_move = true
-
-	# Move player to the front of the hiding stop
+func move_player_back_to_front() -> void:
 	if current_PLACEMENT == PLACEMENT.left:
 		player.global_position.x -= STOP_HIDING_DISPLACEMENT
 	elif current_PLACEMENT == PLACEMENT.right:
