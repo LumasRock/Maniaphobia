44e94ee2de225decd107d1f92efa89d89f2417ec
K3vnDev <kevinrpolo16@gmail.com>
2026-07-14 20:27:26 -0500

    fixed Maw buggy movement

--- Script/Enemy.gd
+++ Script/Enemy.gd
@@ -1,7 +1,15 @@
+class_name Maw
 extends CharacterBody2D
 
 @onready var state_machine = $"State Machine"
-@export var attack_damage: int = 50
+
+@export_group("Attacking")
+@export var attack_damage := 50
+@export var attack_range := 30.0
+
+@export_group("Chasing")
+@export var start_chase_distance := 300.0
+@export var stop_chase_distance := 200.0
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
